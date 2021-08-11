f36287bc8d91422ca17b35a4aa340e6ef609ceb4
Rob Thijssen <rthijssen@gmail.com>
2021-08-11 17:54:20 +0300

    install ubuntu docker from docker repo

--- src/aws.js
+++ src/aws.js
@@ -68,10 +68,10 @@ async function startInstance(label, githubRegistrationToken) {
     `    - mkdir -p ${config.input.runnerInstallDir}`,
     `    - curl -O -L https://github.com/actions/runner/releases/download/v${config.input.runnerVersion}/actions-runner-linux-${config.input.runnerArch}-${config.input.runnerVersion}.tar.gz`,
     `    - tar xfz actions-runner-linux-${config.input.runnerArch}-${config.input.runnerVersion}.tar.gz -C ${config.input.runnerInstallDir} --strip-components=1`,
-    `    - cd ${config.input.runnerInstallDir} && ./config.sh --unattended --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
-    `    - cd ${config.input.runnerInstallDir} && ./svc.sh install`,
-    `    - cd ${config.input.runnerInstallDir} && ./svc.sh start`,
-    `    - cd ${config.input.runnerInstallDir} && ./svc.sh status`,
+    `    - cd ${config.input.runnerInstallDir} && ${config.input.runnerInstallDir}/config.sh --unattended --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
+    `    - cd ${config.input.runnerInstallDir} && ${config.input.runnerInstallDir}/svc.sh install`,
+    `    - cd ${config.input.runnerInstallDir} && ${config.input.runnerInstallDir}/svc.sh start`,
+    `    - cd ${config.input.runnerInstallDir} && ${config.input.runnerInstallDir}/svc.sh status`,
     '',
   ].join('\n');
 
